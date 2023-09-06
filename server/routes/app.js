51d6e0c20572d5f27d92ce9c4cee7aa44cda9030
Kelvin Akaba <akabakelvin@gmail.com>
2023-09-06 23:17:27 +0000

    contact page

--- server/routes/app.js
+++ server/routes/app.js
@@ -55,7 +55,9 @@ router.post('/search', async (req, res) => {
     }
 })
 
-
+router.get('/contact', (req, res) => {
+    res.render('contact')
+})
 
 router.get('/about', (req, res) => {
     res.render('about')
